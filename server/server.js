46878a0500ae9295fd17b35dfbd3dc9c96352119
Tomasz <tmakowski1337@gmail.com>
2018-07-18 16:01:08 +0200

    Add post /todos and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/Todo');
+var {User} = require('./models/User');
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res)=>{
+	var newtodo = new Todo({
+		text: req.body.text
+	});
+
+	newtodo.save().then((doc)=>{
+		res.send(doc);
+	}, (e)=>{
+		res.status(400).send(e);
+	})
+	
+});
+
+app.listen(3000, ()=>{
+	console.log('server has started!');
+});
+
+
+
+
+
+/*var firstUser = new User({
+	email: ''
+});
+
+firstUser.save().then((saved)=>{
+	console.log('Saved user to the Db:', JSON.stringify(saved, undefined, 2));
+}, (err)=>{
+	console.log('Unable to save the user:', err)
+});*/
+
+/*var newTodo = new Todo({
+	text: 'cook dinner',
+	completed: false
+});*/
+
+/*newTodo.save().then((saved)=>{
+	console.log('Saved todo:', saved);
+}, (e)=>{
+	console.log('Unable to save the data');
+});*/
+
