a98bf9993a968cd9d27533eb6829792014965122
Tomasz <tmakowski1337@gmail.com>
2018-07-19 20:58:07 +0200

    Add DELETE /todo/:id route

---
+++ playground/mongodb-remove.js
@@ -0,0 +1,11 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {User} = require('./../server/models/User');
+const {ObjectID} =require('mongodb');
+
+/*Todo.remove({}).then((res)=>{
+	console.log(res);
+})*/
+
+// Todo.findOneAndRemove
+
+//Todo.findByIdAndRemove
--- server/server.js
+++ server/server.js
@@ -42,11 +42,29 @@ app.get('/todos/:id', (req, res)=>{
 	}
 	Todo.findById(id).then((todo)=>{
 		if(!todo){
-			res.status(404).send('user not found');
+			res.status(404).send('Todo not found');
 		}
 		res.send({todo});
 	}).catch((e)=>{res.status(400).send()})
-})
+});
+
+app.delete('/todos/:id', (req,res)=>{
+	var id = req.params.id;
+	
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	Todo.findByIdAndRemove(id).then((todo)=>{
+		if(!todo){
+			return res.status(404).send()
+		}
+		return res.status(200).send()
+		
+
+	}).catch((e)=>{res.status(400).send()})
+
+});
 
 app.listen(port, ()=>{
 	console.log(`Server has started at port ${port}`);
